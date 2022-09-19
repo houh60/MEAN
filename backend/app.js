435581eaeb9e1afe085d7fb3e32689ed769c019a
William Hou <williamhou38@yahoo.com>
2022-09-19 09:28:40 -0500

    new changes

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://William:SfnPbwaVfdYD6C0x@cluster0.gkvfa.mongodb.net/node-angular?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://William:'
+    + process.env.MONGO_ATLAS_PW
+    + '@cluster0.gkvfa.mongodb.net/node-angular?retryWrites=true&w=majority')
     .then(() => {
         console.log('Connected to database!');
     }).catch(() => {
